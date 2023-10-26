f044e8e6f39ef909b14bc9c994d1d9bbde930231
jbg <joaobenine@gmail.com>
2023-10-26 11:19:25 -0300

    config styles modal

--- src/app/ui/componentes/modalContent.tsx
+++ src/app/ui/componentes/modalContent.tsx
@@ -16,12 +16,26 @@ interface ModalViewContentProps {
 export function ModalViewContent(propsModal: ModalViewContentProps) {
   return (
     <>
-      <Modal isOpen={propsModal.isOpen} onClose={propsModal.onClose}>
-        <ModalContent>
-          <ModalHeader>Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>Conteudo</ModalBody>
-
+      <Modal
+        isOpen={propsModal.isOpen}
+        onClose={propsModal.onClose}
+        size="xs"
+        scrollBehavior="inside"
+      >
+        <ModalContent
+          className="modalClass"
+          position="absolute"
+          right={0}
+          h="full"
+          height="full"
+          boxShadow="2xl"
+          borderRadius="3xl"
+          border="ActiveBorder"
+        >
+          <ModalHeader>
+            <ModalCloseButton left="0.5" />
+          </ModalHeader>
+          <ModalBody py="5">Conteudo</ModalBody>
           <ModalFooter></ModalFooter>
         </ModalContent>
       </Modal>
