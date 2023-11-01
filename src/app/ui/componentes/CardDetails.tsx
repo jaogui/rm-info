adfe13a085874f6539fdca614a6cfd5a2ef7f890
jbg <joaobenine@gmail.com>
2023-11-01 02:00:31 -0300

    create component for details card

---
+++ src/app/ui/componentes/CardDetails.tsx
@@ -0,0 +1,24 @@
+import { Badge, Text } from "@chakra-ui/react";
+
+interface DetailsCardProps {
+  content: string;
+  labelInfo: string;
+}
+
+export function CardDetails(props: DetailsCardProps) {
+  return (
+    <div className="flex gap-2 items-center max-w-[170px]">
+      <Badge fontSize={11} colorScheme="cyan">
+        {props.labelInfo}
+      </Badge>
+      <Text
+        fontSize={13}
+        textAlign="center"
+        fontWeight="normal"
+        className="truncate"
+      >
+        {props.content}
+      </Text>
+    </div>
+  );
+}
