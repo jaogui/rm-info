519d954555f74e6f002840a3120126ae0897ef70
jbg <joaobenine@gmail.com>
2023-11-07 20:44:04 -0300

    transformando botão loading data em componente e gerenciado estado para visualização ux

---
+++ src/app/ui/componentes/ButtonLoad.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Button } from "@chakra-ui/react";
+import { Plus } from "lucide-react";
+import { useContextData } from "../../data/hooks/useContextData";
+
+export function ButtonLoad() {
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const { handleLoadMore } = useContextData();
+
+  function handleBtn() {
+    try {
+      handleLoadMore();
+      setIsLoadingMore(true);
+      setTimeout(() => {
+        setIsLoadingMore(false); 
+      }, 100); 
+    } catch (error) {
+      console.error("Erro ao carregar mais dados:", error);
+    }
+  }
+
+  return (
+    <Button
+      colorScheme="cyan"
+      margin="auto"
+      borderRadius="full"
+      position="fixed"
+      bottom="30px"
+      onClick={handleBtn}
+      disabled={isLoadingMore}
+    >
+      <p className="text-white">{isLoadingMore ? "Loading.." : <Plus />}</p>
+    </Button>
+  );
+}
