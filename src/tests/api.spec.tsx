3f8d6b738c9eed5cc75995bc5e0a3ef6f18c3288
jbg <joaobenine@gmail.com>
2023-11-11 01:57:51 -0300

    test init load data api

--- src/tests/api.spec.tsx
+++ src/tests/api.spec.tsx
@@ -1,28 +1,54 @@
 import Home from "../app/page";
 import { ContextData } from "../app/data/hooks/ContextData";
 import { render, waitFor, screen } from "@testing-library/react";
+import fetchMock from "fetch-mock";
 
-describe("test data api", () => {
-  it("should return correct data", async () => {
+describe("App", () => {
+  beforeEach(() => {
+    fetchMock.restore();
+  });
+
+  test("render app for api success", async () => {
+    const fakeData = [
+      { id: 1, name: "Rick Sanchez"},
+      { id: 2, name: "Mory Smith"},
+    ];
+
     global.fetch = jest.fn().mockResolvedValue({
-      json: jest.fn().mockResolvedValue({
-        name: "Rick Sanchez",
-        status: "Alive",
-        species: "Human",
-        gender: "Male",
-      }),
+      status: 200,
+      fakeData,
     });
 
     render(
       <ContextData>
         <Home />
       </ContextData>
-    )
+    );
 
-    await waitFor(() => {
-      const nameElement = screen.getByText("Morty Smith");
-      expect(nameElement).toBeInTheDocument();
+    // await waitFor(() => {
+    //   expect(screen.getByText('Rick Sanchez')).toBeInTheDocument()
+    // })
 
-    });
+    expect(await screen.findByText('Rick Sanchez')).toBeInTheDocument()
+  
+    
+
+    // describe("test data api", () => {
+    //   it("should return correct data", async () => {
+    //     global.fetch = jest.fn().mockResolvedValue({
+    //       json: jest.fn().mockResolvedValue({
+
+    //       }),
+    //     });
+
+    // render(
+    //   <ContextData>
+    //     <Home />
+    //   </ContextData>
+    // )
+
+    // await waitFor(() => {
+    //   const nameElement = screen.getByText("Morty Smith");
+    //   expect(nameElement).toBeInTheDocument();
   });
 });
