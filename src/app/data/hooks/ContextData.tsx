d5a78e275c4676ed2d34e7f5bc2a7ad8fa44d5c7
jbg <joaobenine@gmail.com>
2023-11-25 21:01:31 -0300

    init search fetch context

--- src/app/data/hooks/ContextData.tsx
+++ src/app/data/hooks/ContextData.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { createContext, ReactNode, useEffect, useState } from "react";
+import { createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
 import { fetchDefault } from "../api/axiosConfig";
 
 export interface CharacterData {
@@ -21,28 +21,33 @@ export interface CharacterData {
   };
 }
 
-
 interface DataContextProps {
   children: ReactNode;
 }
 
 interface DataContextValue {
- characterDataFetch: CharacterData[];
- handleLoadMore: () => void;
- isLoading: boolean;
+  characterDataFetch: CharacterData[];
+  handleLoadMore: () => void;
+  isLoading: boolean;
+  setSearchInputQuery: Dispatch<SetStateAction<string>>;
+  searchCharacterData:  CharacterData[];
 }
 
 export const DataContext = createContext<DataContextValue | null>(null);
 
 export function ContextData({ children }: DataContextProps) {
   const [characterDataFetch, setCharacterDataFetch] = useState<CharacterData[]>([]);
+  const [searchCharacterData, setSearchCharacterData] = useState<CharacterData[]>([]);
+  const [searchInputQuery, setSearchInputQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchCharacter() {
       try {
-        const response = await fetchDefault.get(`/character?page=${currentPage}`);
+        const response = await fetchDefault.get(
+          `/character?page=${currentPage}`
+        );
         const data = await response.data.results;
         setCharacterDataFetch((prevData) => {
           const newResults = data.filter((newResult: CharacterData) => {
@@ -53,21 +58,39 @@ export function ContextData({ children }: DataContextProps) {
           });
           return [...prevData, ...newResults];
         });
-        setIsLoading(false)
+        setIsLoading(false);
       } catch (error) {
         console.error("Request error", error);
       }
     }
     fetchCharacter();
-  },[currentPage]);
+  }, [currentPage]);
 
-  function handleLoadMore(){
-    setIsLoading(false)
+
+  useEffect(() => {
+    async function fetchSearch() {
+      try {
+        const response = await fetchDefault(`https://rickandmortyapi.com/api/character/?name=${searchInputQuery}`
+        );
+        const data = response.data.results;
+        console.log(data)
+        setSearchCharacterData(data)
+      } catch (error) {
+        console.error("Error search request", error);
+      }
+    }
+    fetchSearch()
+  }, [searchInputQuery]);
+
+  function handleLoadMore() {
+    setIsLoading(false);
     setCurrentPage((prevPage) => prevPage + 1);
-  };
+  }
 
   return (
-    <DataContext.Provider value={{ characterDataFetch, handleLoadMore, isLoading }}>
+    <DataContext.Provider
+      value={{ characterDataFetch, handleLoadMore, isLoading, setSearchInputQuery, searchCharacterData }}
+    >
       {children}
     </DataContext.Provider>
   );
